2cec585c3417f87741ddbca6baee2616bc94e296
Vladimil Jimenez <jsvladimil@gmail.com>
2023-03-14 14:55:49 -0400

    Add navbar layout

---
+++ exchange-app/src/components/layouts/Navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export const Navbar = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <div className="bg-white h-16 w-full flex justify-center items-center shadow">
+      <div className="max-w-7xl w-full p-3 flex justify-between items-center">
+        <h3 className="text-xl text-blue-900">Exchange APP</h3>
+        <div className="flex space-x-1 items-center transition-all duration-300 ease-in-out">
+          <Link
+            href="/"
+            className={`hover:bg-gray-200 p-3 rounded-lg hover:text-blue-600 ${
+              pathname == "/" ? "text-blue-600" : "text-gray-500"
+            }`}
+          >
+            Home
+          </Link>
+          <Link
+            href="/stored"
+            className={`hover:bg-gray-200 p-3 rounded-lg hover:text-blue-600 ${
+              pathname == "/stored" ? "text-blue-600" : "text-gray-500"
+            }`}
+          >
+            Stored
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
--- exchange-app/src/pages/_app.tsx
+++ exchange-app/src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import { Navbar } from "@/components/layouts";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="h-screen flex-col">
+      <Navbar />
+      <main className="flex-grow p-4">
+        <div className="mx-auto max-w-7xl p-3">
+          <Component {...pageProps} />
+        </div>
+      </main>
+    </div>
+  );
 }
